3b794a28fdb807dee7f5fb85479e94e64d405efa
ruviRedner <r.y.r7966@gmail.com>
2026-01-25 21:21:47 +0200

    change env

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,18 +1,13 @@
 import "dotenv/config";
 
-function must(key: string) {
-  const v = process.env[key];
-  if (!v) throw new Error(`Missing env: ${key}`);
-  return v;
-}
 
 export const env = {
   port: Number(process.env.PORT ?? 4000),
-  mongoUri: must("MONGO_URI"),
-  jwtSecret: must("JWT_SECRET"),
+  mongoUri: process.env.MONGO_URI ?? "mongodb+srv://ruby:7966@cluster0.xjvgc.mongodb.net/?appName=supermarketPro",
+  jwtSecret: process.env.JWT_SECRET ?? "super-secret-change-me",
   jwtExpiresIn: process.env.JWT_EXPIRES_IN ?? "7d",
 
-  adminEmail: must("ADMIN_EMAIL"),
-  adminPassword: must("ADMIN_PASSWORD"),
+  adminEmail: process.env.ADMIN_EMAIL ?? "admin@example.com",
+  adminPassword: process.env.ADMIN_PASSWORD ?? "theHook7966",
   adminName: process.env.ADMIN_NAME ?? "Admin",
 };
